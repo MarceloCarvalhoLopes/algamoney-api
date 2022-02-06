db00aa68976bf416ded210578335bab26db8289f
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2022-02-05 21:35:31 -0300

    Creating a new Category with POST

--- backend/src/main/java/com/algaworks/algamoney/api/resource/CategoryResource.java
+++ backend/src/main/java/com/algaworks/algamoney/api/resource/CategoryResource.java
@@ -1,11 +1,19 @@
 package com.algaworks.algamoney.api.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.algaworks.algamoney.api.model.Category;
 import com.algaworks.algamoney.api.repository.CategoryRepository;
@@ -20,5 +28,21 @@ public class CategoryResource {
 	@GetMapping
 	public List<Category> list() {
 		return categoryRepository.findAll();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Category> create(@RequestBody Category category, HttpServletResponse response) {
+		Category categorySaved = categoryRepository.save(category);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+				.buildAndExpand(categorySaved.getId()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categorySaved);
+	}
+	
+	@GetMapping("/{id}")
+	public Category findById(@PathVariable long id) {
+		return categoryRepository.findById(id).orElse(null);
 	}
 }
