dacf08d334003b69c6e25fb7da29660f3a4123f7
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2022-03-14 17:07:29 -0300

    Handling errors with ExceptionHandler

--- backend/src/main/java/com/algaworks/algamoney/api/AlgamoneyApplication.java
+++ backend/src/main/java/com/algaworks/algamoney/api/AlgamoneyApplication.java
@@ -2,6 +2,8 @@ package com.algaworks.algamoney.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class AlgamoneyApplication {
@@ -9,5 +11,7 @@ public class AlgamoneyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AlgamoneyApplication.class, args);
 	}
+	
+
 
 }
