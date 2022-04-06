a65a2d6b5c9e4d4f0a17b6270d5cf8474856ed37
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2022-04-06 08:50:23 -0300

    Deleting Launching

--- backend/src/main/java/com/algaworks/algamoney/api/resource/LaunchingResource.java
+++ backend/src/main/java/com/algaworks/algamoney/api/resource/LaunchingResource.java
@@ -12,12 +12,14 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algamoney.api.event.ResourceCreatedEvent;
@@ -63,6 +65,12 @@ public class LaunchingResource {
 		applicationEventPublisher.publishEvent(new ResourceCreatedEvent(this, respose, savedLaunching.getId()));
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(savedLaunching);
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+		launchingRepository.deleteById(id);
 	}
 	
 	@ExceptionHandler({NonExistentOrActivePersonException.class})
