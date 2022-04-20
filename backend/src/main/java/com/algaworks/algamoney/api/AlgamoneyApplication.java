0a628040b00c8c582e55881cd92ce26ce3881fb6
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2022-04-20 13:57:28 -0300

    Spring profiles

--- backend/src/main/java/com/algaworks/algamoney/api/AlgamoneyApplication.java
+++ backend/src/main/java/com/algaworks/algamoney/api/AlgamoneyApplication.java
@@ -2,10 +2,12 @@ package com.algaworks.algamoney.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.algaworks.algamoney.api.config.property.AlgamoneyApiProperty;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AlgamoneyApiProperty.class)
 public class AlgamoneyApplication {
 
 	public static void main(String[] args) {
--- backend/src/main/java/com/algaworks/algamoney/api/cors/CorsFilter.java
+++ backend/src/main/java/com/algaworks/algamoney/api/cors/CorsFilter.java
@@ -10,16 +10,23 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import com.algaworks.algamoney.api.config.property.AlgamoneyApiProperty;
+
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
-
-	private String allowedOrigin = "http://localhost:8000"; //TODO: Configurar para diferentes ambientes 
 	
+	@Autowired
+	private AlgamoneyApiProperty algamoneyApiProperty;
+
+	
+	
+
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
 			throws IOException, ServletException {
@@ -27,10 +34,10 @@ public class CorsFilter implements Filter {
 		HttpServletRequest request = (HttpServletRequest) req;
 		HttpServletResponse response = (HttpServletResponse) resp;
 		
-		response.setHeader("Access-Control-Allow-Origin", allowedOrigin);
+		response.setHeader("Access-Control-Allow-Origin", algamoneyApiProperty.getAllowedOrigin());
 		response.setHeader("Access-Control-Allow-Credentials", "true");
 		
-		if ("OPTIONS".equals(request.getMethod()) && allowedOrigin.equals(request.getHeader("Origin"))) {
+		if ("OPTIONS".equals(request.getMethod()) && algamoneyApiProperty.getAllowedOrigin().equals(request.getHeader("Origin"))) {
 			
 			response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
 			response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
