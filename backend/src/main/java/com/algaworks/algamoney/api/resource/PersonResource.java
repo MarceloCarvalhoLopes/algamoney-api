cf045a1843d9e9b9821b1aeb798178d3119926de
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2022-04-26 21:18:34 -0300

    Research person

--- backend/src/main/java/com/algaworks/algamoney/api/resource/PersonResource.java
+++ backend/src/main/java/com/algaworks/algamoney/api/resource/PersonResource.java
@@ -1,12 +1,12 @@
 package com.algaworks.algamoney.api.resource;
 
-import java.util.List;
-
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,9 +41,16 @@ public class PersonResource {
 	
 	@GetMapping
 	@PreAuthorize("hasAuthority('ROLE_RESEARCH_PERSON') and hasAuthority('SCOPE_read')")
-	public List<Person>list(){
-		return personRepository.findAll();
+	public Page<Person> research(@RequestParam(required = false, defaultValue = "") String name, Pageable pageable){
+		return personRepository.findByNameContaining(name, pageable);
 	}
+
+	
+//	@GetMapping
+//	@PreAuthorize("hasAuthority('ROLE_RESEARCH_PERSON') and hasAuthority('SCOPE_read')")
+//	public List<Person>list(){
+//		return personRepository.findAll();
+//	}
 	
 	@GetMapping("/{id}")
 	@PreAuthorize("hasAuthority('ROLE_RESEARCH_PERSON') and hasAuthority('SCOPE_read')")
